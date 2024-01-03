a114fde5f9e97dbd2eec376e9796a917d1ac3bb0
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-03 22:22:28 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {Routes, Route} from 'react-router-dom';
+import './globals.css'
+const App = () => {
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            {/* public routes */}
+            <Route path="/sign-in" element={<SignInForm/>}/>
+
+            {/* private routes */}
+            <Route index element={<Home />} />
+        </Routes>
+    </main>
+  )
+}
+
+export default App
