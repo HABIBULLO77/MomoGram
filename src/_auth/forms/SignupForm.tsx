e8758d7e151bcc4bed3185576561c1778a848310
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-08 23:01:52 +0900

    Auth pages(edition globals.css and tailwind.config.js file edit)

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -17,7 +17,7 @@ import { z } from "zod"
  
 
 const SignupForm = () => {
-  
+  const isLoading = true;
 
    // 1. Define your form.
    const form = useForm<z.infer<typeof SignupValidation>>({
@@ -40,8 +40,24 @@ const SignupForm = () => {
       <Form {...form}>
         <div className="sm:w-420 flex-center flex-col">
           <img src="/assets/images/logo.svg" alt="logo"/>
-        </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">To use Momogram enter your details</p>
+        
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input"{...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="username"
@@ -49,17 +65,41 @@ const SignupForm = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input type="text" className="shad-input"{...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" className="shad-input"{...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input"{...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">Submit</Button>
       </form>
+      </div>
     </Form>
 
   )
