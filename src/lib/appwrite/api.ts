9b0b718825571dbbbb61b7a640991cefdce9b6ec
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-10 12:41:05 +0900

    Auth functionality

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,20 @@
-export async function createUserAccount(user){
-    
+import { INewUser } from "@/types";
+import { ID} from 'appwrite';
+import { account } from "./config";
+
+export async function createUserAccount(user: INewUser){
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.name,
+            user.email,
+            user.password,
+        )
+
+        return newAccount;
+        
+    } catch (error) {
+        console.log('Error creating user account', error);
+        return error; 
+    }
 }
