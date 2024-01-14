ab3cb5f882e05cb97a75412c59aabcc1adc7dc61
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-14 22:11:32 +0900

    Auth Context edit

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { getCurrentUser } from '@/lib/appwrite/api';
-import { IContextType } from '@/types';
+import { IContextType, IUser } from '@/types';
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export const INITIAL_USER = {
     id: '',
@@ -27,9 +28,27 @@ const AuthProvider = ({children}: {children: React.ReactNode}) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
+  const navigate = useNavigate();
+
   const checkAuthUser = async () => {
     try {
       const currentAccount = await getCurrentUser();
+
+      if(currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        })
+
+        setIsAuthenticated(true);
+        return true;
+      }
+      return false;
+
     } catch (error) {
       console.log(error);
       return false;
@@ -38,7 +57,16 @@ const AuthProvider = ({children}: {children: React.ReactNode}) => {
     }
   };
 
-  const value{
+  useEffect(() => {
+    if(
+      localStorage.getItem('cookieFallback') === '[]' ||
+      localStorage.getItem('cookieFallback') === null
+    ) navigate('/sign-in')
+
+    checkAuthUser();
+  }, [])
+
+  const value ={
     user,
     setUser,
     isLoading,
@@ -54,4 +82,6 @@ const AuthProvider = ({children}: {children: React.ReactNode}) => {
   )
 }
 
-export default AuthContext
+export default AuthProvider
+
+export const useUserContext = () => useContext(AuthContext);
