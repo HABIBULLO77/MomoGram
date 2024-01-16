b1040aa72697e9e42fb2f46789c7bad08ac5f0a7
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-16 21:07:31 +0900

    singIn page update

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -12,10 +12,10 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { SignupValidation as SigninValidation } from "@/lib/validation"
+import {  SigninValidation } from "@/lib/validation"
 import { z } from "zod"
 import Loader from "@/components/shared/Loader"
-import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
+import {  useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useToast } from "@/components/ui/use-toast"
 import { useUserContext } from "@/context/AuthContext"
 
@@ -26,7 +26,7 @@ const SigninForm = () => {
   const {checkAuthUser, isLoading: isUserLoading} = useUserContext();
   const navigate = useNavigate();
 
-  const { mutateAsync: signInAccount, isPending: isSigningInUser} = useSignInAccount();
+  const { mutateAsync: signInAccount,} = useSignInAccount();
 
    // 1. Define your form.
    const form = useForm<z.infer<typeof SigninValidation>>({
@@ -64,36 +64,11 @@ const SigninForm = () => {
         <div className="sm:w-420 flex-center flex-col">
           <img src="/assets/images/momo.svg" alt="logo"/>
 
-          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-          <p className="text-light-3 small-medium md:base-regular mt-2">To use Momogram enter your details</p>
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">Welcome back! Please enter your details</p>
         
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input"{...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input"{...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+       
         <FormField
           control={form.control}
           name="email"
@@ -121,16 +96,16 @@ const SigninForm = () => {
           )}
         />
         <Button type="submit" className="shad-button_primary">
-            {isCreatingAccount? (
+            {isUserLoading? (
               <div className="flex-center gap-2">
                 <Loader/>Loading...
               </div>
-            ): "Sign up"}
+            ): "Sign in"}
         </Button>
 
         <p className="text-small-regular text-light-2 text-center mt-2">
-          Already have an account?
-          <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          Don't have an account?
+          <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1">Sign up</Link>
         </p>
       </form>
       </div>
