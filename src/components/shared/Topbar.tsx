6d7c076003d2a3507f3bbe4fadc1b8ca3535391a
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-19 12:40:43 +0900

    profile photo edit

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -2,10 +2,13 @@ import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useEffect } from 'react';
+import { useUserContext } from '@/context/AuthContext';
 
 const Topbar = () => {
   const {mutate: signOut, isSuccess} = useSignOutAccount();
   const navigate = useNavigate();
+  const { user } = useUserContext();
+
   useEffect( ()=> {
     if (isSuccess) navigate(0)
   }, [isSuccess])
@@ -22,6 +25,9 @@ const Topbar = () => {
           <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut()}>
             <img src='/assets/icons/logout.svg' alt='logout'/>
           </Button>
+          <Link to={'/profile/${user.id}'} className='flex-center gap-3'>
+            <img src={user.imageUrl || '/assets/images/profile-placeholder.svg'} alt="profile" className='h-8 w-8 rounded-full' />
+          </Link>
         </div>
       </div>
     </section>
