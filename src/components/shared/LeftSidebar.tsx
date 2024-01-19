da307c02d63b2d0b5b6de6a4a7995d690efb7124
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-19 20:25:45 +0900

    constants->index.ts and LeftSidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
 import { useEffect } from 'react';
 import { useUserContext } from '@/context/AuthContext';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
 
 const LeftSidebar = () => {
   const {mutate: signOut, isSuccess} = useSignOutAccount();
@@ -32,6 +34,18 @@ const LeftSidebar = () => {
           </p>
         </div>
         </Link>
+        <ul className='flex flex-col gap-6'>
+          {sidebarLinks.map((link: INavLink) =>{
+            return(
+              <li key={link.label} className='leftsidebar-link'>
+                <NavLink to={link.route}>
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul>
+
       </div>
     </nav>
   )
