fc9ab126c7f83e5f6f787f15b8f8f546c6fbb33f
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-01-21 21:45:43 +0900

    LeftSidebar Logout

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -50,6 +50,10 @@ const LeftSidebar = () => {
         </ul>
 
       </div>
+      <Button variant="ghost" className='shad-button_ghost' onClick={() => signOut()}>
+        <img src='/assets/icons/logout.svg' alt='logout'/>
+        <p className='small-medium lg:base-medium'>Logout</p>
+       </Button>
     </nav>
   )
 }
