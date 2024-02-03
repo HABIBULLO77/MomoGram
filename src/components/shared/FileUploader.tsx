d7775f5b8d9f18bfc5ca4687f3564b12cc93c755
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-02-03 12:59:49 +0900

    FileUploader edit

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,14 +1,28 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 const FileUploader = () => {
+  const [fileUrl, setFileUrl] = useState('')
 
   const onDrop = useCallback(acceptedFiles => {
     // Do something with the files
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop}) 
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
+      <input {...getInputProps()} className='cursor-pointer'/>
+      {
+        fileUrl ? (
+          <div>
+
+          </div>
+        ) : (
+          <div className='file_uploader-box'>
+            <img src="/assets/icons/file-upload.svg" width={96} height={77} alt="file-uploader" />
+          </div>
+        )
+      }
+    </div>
   )
 }
 
-export default FileUploader
+export default FileUploader 
